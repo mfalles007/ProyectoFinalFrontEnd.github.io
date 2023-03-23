120195e8e4d1abec2c16e559d5487a4f8fba360e
mfalles007 <108810443+mfalles007@users.noreply.github.com>
2023-03-23 13:40:44 -0300

    cambios en gral
    cambios imagenes,colores, datos..

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
-}
 
 
 
